969c6dc08ebbf5fed5535c999e1a52c4d68d44fa
Hwiseong <hw969655@dal.ca>
2025-01-18 14:51:48 -0400

    merged task and hobby part

--- mentalhealth-ai/src/App.jsx
+++ mentalhealth-ai/src/App.jsx
@@ -1,23 +1,51 @@
 import { useState } from 'react'
 import './App.css'
 import { testController } from './controllers/GeminiController.js';
-import Survey from './components/Survey.jsx';
 import TasksSurvey from './components/TasksSurvey.jsx';
 import HobbiesSurvey from './components/HobbiesSurvey.jsx';
 
 function App() {
     const [hobby, setHobby] = useState('');
     const [hobbies, setHobbies] = useState([]);
-    return (
-        <>
-            <HobbiesSurvey 
-            hobby={hobby}
-            hobbies={hobbies}
-            setHobby={setHobby}
-            setHobbies={setHobbies}
-            />
-        </>
-    );
+    const [tasksDone, setTasksDone] = useState(false);
+    const [task, setTask] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const [level, setLevel] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    
+    if (!tasksDone) {
+        return (
+            <>
+            <div className='col-md-12'>
+                <TasksSurvey 
+                    task={task}
+                    setTask={setTask}
+                    level={level}
+                    setLevel={setLevel}
+                    dueDate={dueDate}
+                    setDueDate={setDueDate}
+                    tasks={tasks}
+                    setTasks={setTasks}
+                />
+            </div>
+                
+            </>
+        );
+    }
+    else {
+        return (
+            <>
+                <HobbiesSurvey 
+                    hobby={hobby}
+                    hobbies={hobbies}
+                    setHobby={setHobby}
+                    setHobbies={setHobbies}
+                />
+            </>
+            );
+    }
+    
 }
 
 export default App;
