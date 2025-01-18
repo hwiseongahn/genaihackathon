286cab8b6e8c0782130644b79932dbc19ca1b6b2
lennartschaeffer <ln546006@dal.ca>
2025-01-18 12:54:48 -0400

    pulling dev into my branch

--- mentalhealth-ai/src/App.jsx
+++ mentalhealth-ai/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { testController } from './controllers/GeminiConroller'
+import { testController } from './controllers/GeminiController'
 
 function App() {
   const [count, setCount] = useState(0)
--- mentalhealth-ai/src/controllers/GeminiController.js
+++ mentalhealth-ai/src/controllers/GeminiController.js
@@ -1,20 +1,31 @@
 import axios from "axios";
-export const testController = () => {
- console.log('Hello from GeminiController');
-  axios.post(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${
-      import.meta.env.VITE_API_KEY
-    }`,
-    {
-      contents: [
-        {
-          parts: [{ text: "Explain how AI works" }],
-        },
-      ],
-    }
-  ).then((res) => {
-    console.log(res.data.candidates[0].content.parts[0].text);
-  }).catch((err) => {
-    console.error(err);
-  });
+export const testController = async () => {
+  console.log("Hello from GeminiController");
+  try {
+    const res = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${
+        import.meta.env.VITE_API_KEY
+      }`,
+      {
+        contents: [
+          {
+            parts: [
+              {
+                text: "give me a task list including times of the day in a JSON format so i can easily map it in my react front-end.",
+              },
+            ],
+          },
+        ],
+      }
+    );
+    const raw_response = res.data.candidates[0].content.parts[0].text;
+    const cleaned_response = raw_response
+      .replace("```json", "")
+      .replace("```", "")
+      .trim();
+    const result = JSON.parse(cleaned_response);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
