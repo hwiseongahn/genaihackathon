31190e8618f3e7ffdbe18bb2c2c8c74c9c409de3
Kevin Nguyen <kv375322@dal.ca>
2025-01-18 14:41:35 -0400

    Pushed almost done Hobby Survey

--- mentalhealth-ai/src/App.jsx
+++ mentalhealth-ai/src/App.jsx
@@ -3,12 +3,18 @@ import './App.css'
 import { testController } from './controllers/GeminiController.js';
 import Survey from './components/Survey.jsx';
 import TasksSurvey from './components/TasksSurvey.jsx';
+import HobbiesSurvey from './components/HobbiesSurvey.jsx';
 
 function App() {
-
+    const [hobby, setHobby] = useState('');
+    const [hobbies, setHobbies] = useState([]);
     return (
         <>
-            <TasksSurvey 
+            <HobbiesSurvey 
+            hobby={hobby}
+            hobbies={hobbies}
+            setHobby={setHobby}
+            setHobbies={setHobbies}
             />
         </>
     );
